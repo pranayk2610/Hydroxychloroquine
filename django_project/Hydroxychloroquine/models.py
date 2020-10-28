72712d48674e418915816ce8d50c8140d2b7e9f1
cjb35165 <43619815+cjb35165@users.noreply.github.com>
2020-10-28 17:05:38 -0400

    Added building and excursion models
    Added building and excursion models. Also updated CustomUser to include other attributes

--- django_project/Hydroxychloroquine/models.py
+++ django_project/Hydroxychloroquine/models.py
@@ -8,6 +8,7 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    display_name = models.CharField(max_length = 128)
     email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -23,9 +24,28 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 
 class Report(models.Model):
-    date_of_test = models.DateTimeField('')
-    date_last_on_campus = models.DateTimeField('')
+    date_of_test = models.DateTimeField()
+    date_last_on_campus = models.DateTimeField()
     user_id = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
 
     def __str__(self):
         return "%s %s %s" % (self.date_of_test, self.date_last_on_campus, self.user_id)
+
+class Building(models.Model):
+    building_id = models.IntegerField(primary_key = True)
+    building_name = models.CharField(max_length = 128)
+        
+    def __str__(self):
+        return "%s %s" % (self.building_id, self.building_name)
+
+class Excursion(models.Model):
+    report_id = models.ForeignKey(Report, on_delete = models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    building_id = models.ForeignKey(Building, on_delete = models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return "%s %s" % (self.report_id, self.start_time, self.end_time)
+
+
