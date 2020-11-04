3618a751ac439a574bf65b11946b73df50584be4
pranayk2610 <pranaykumar2610@gmail.com>
2020-11-03 20:48:07 -0500

    adds profile editing

--- django_project/Hydroxychloroquine/views.py
+++ django_project/Hydroxychloroquine/views.py
@@ -6,6 +6,7 @@ from django_email_verification import sendConfirm
 from django.http import HttpResponse, HttpResponseRedirect
 from .testingVars import test_buildings, test_reports
 from . import forms
+
 # from . import view_templates
 
 # for display purposes
@@ -40,23 +41,24 @@ display_times = [
     "11:00PM",
 ]
 
+
 def home(request):
     context = {
         "title": "home",
         "recent_reports": test_reports,
-        }
+    }
     return render(request, "Hydroxychloroquine/home.html", context)
+
 
 @login_required
 def account(request):
     if request.method == "POST":
-        form = forms.CustomUserChangeForm(request.POST)
+        form = forms.CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
+            form.save(commit=True)
             return redirect("Hydroxychloroquine-account")
     else:
-        form = forms.CustomUserChangeForm()
+        form = forms.CustomUserChangeForm(instance=request.user)
     context = {
         "title": "account",
         "buildings": test_buildings,
@@ -66,6 +68,7 @@ def account(request):
         "form": form,
     }
     return render(request, "Hydroxychloroquine/account.html", context)
+
 
 def reportTest(request):
     if request.method == "POST":
@@ -85,11 +88,13 @@ def reportTest(request):
     }
     return render(request, "Hydroxychloroquine/reportTest.html", context)
 
+
 def selectBuildings(request):
     context = {
         "title": "selectBuildings",
-        }
+    }
     return render(request, "Hydroxychloroquine/selectBuildings.html", context)
+
 
 def signup(request):
     if request.method == "POST":
@@ -104,52 +109,86 @@ def signup(request):
         form = forms.UserRegistrationForm()
     return render(request, "Hydroxychloroquine/signup.html", {"form": form})
 
+
 def forgotPassword(request):
     context = {
         "title": "forgotPassword",
     }
     return render(request, "Hydroxychloroquine/forgotPassword.html", context)
 
+
 # view template
 def login(request, *args, **kwargs):
-    if request.method == 'POST':
-        if not request.POST.get('RememberMe', None):
+    if request.method == "POST":
+        if not request.POST.get("RememberMe", None):
             request.session.set_expiry(0)
 
     # debug print to terminal start
-    if request.method == 'POST':
-        if not request.POST.get('RememberMe', None):
+    if request.method == "POST":
+        if not request.POST.get("RememberMe", None):
             print("RememberMe was unchecked")
         else:
             print("RememberMe was checked")
-        print(request.POST.get('RememberMe'))
+        print(request.POST.get("RememberMe"))
         print("Login expires in {} seconds".format(request.session.get_expiry_age()))
-        print("Login expires at browser close:",request.session.get_expire_at_browser_close())
+        print(
+            "Login expires at browser close:",
+            request.session.get_expire_at_browser_close(),
+        )
     # debug print to terminal end
 
-    customRender=auth_views.LoginView.as_view(
+    customRender = auth_views.LoginView.as_view(
         template_name="Hydroxychloroquine/login.html",
         # redirect_authenticated_user=True,
         authentication_form=forms.CustomAuthenticationForm,
-        )
+    )
     return customRender(request, *args, **kwargs)
+
 
 def logout(request, *args, **kwargs):
-    customRender=auth_views.LogoutView.as_view(template_name="Hydroxychloroquine/logout.html")
+    customRender = auth_views.LogoutView.as_view(
+        template_name="Hydroxychloroquine/logout.html"
+    )
     return customRender(request, *args, **kwargs)
+
 
 def passwordReset(request, *args, **kwargs):
-    customRender=auth_views.LogoutView.as_view(template_name="Hydroxychloroquine/forgotPassword.html")
+    customRender = auth_views.PasswordResetView.as_view(
+        template_name="Hydroxychloroquine/forgotPassword.html"
+    )
     return customRender(request, *args, **kwargs)
 
-def passwordResetdone(request, *args, **kwargs):
-    customRender=auth_views.LogoutView.as_view(template_name="Hydroxychloroquine/passwordResetDone.html")
+
+def passwordResetDone(request, *args, **kwargs):
+    customRender = auth_views.PasswordResetDoneView.as_view(
+        template_name="Hydroxychloroquine/passwordResetDone.html"
+    )
     return customRender(request, *args, **kwargs)
+
 
 def passwordResetConfirm(request, *args, **kwargs):
-    customRender=auth_views.LogoutView.as_view(template_name="Hydroxychloroquine/passwordResetConfirm.html")
+    customRender = auth_views.PasswordResetConfirmView.as_view(
+        template_name="Hydroxychloroquine/passwordResetConfirm.html"
+    )
     return customRender(request, *args, **kwargs)
 
+
 def passwordResetComplete(request, *args, **kwargs):
-    customRender=auth_views.LogoutView.as_view(template_name="Hydroxychloroquine/passwordResetComplete.html")
+    customRender = auth_views.PasswordResetCompleteView.as_view(
+        template_name="Hydroxychloroquine/passwordResetComplete.html"
+    )
+    return customRender(request, *args, **kwargs)
+
+
+def passwordChange(request, *args, **kwargs):
+    customRender = auth_views.PasswordChangeView.as_view(
+        template_name="Hydroxychloroquine/passwordChange.html"
+    )
+    return customRender(request, *args, **kwargs)
+
+
+def passwordChangeDone(request, *args, **kwargs):
+    customRender = auth_views.PasswordChangeDoneView.as_view(
+        template_name="Hydroxychloroquine/passwordChangeDone.html"
+    )
     return customRender(request, *args, **kwargs)
